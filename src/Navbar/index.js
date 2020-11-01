a680044f340a49389db257ea80348b504724a110
HarshaVardhanNakkina <harshavardhan.n@yahoo.com>
2020-11-01 12:01:18 +0530

    :sparkles: sticky navbar

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -6,28 +6,22 @@ import styles from './navbar.module.scss'
 const Navbar = () => {
 	return (
 		<nav className={styles.navbar}>
-			<ul className={styles.navbar_items_list}>
-				<li className={styles.nav_item}>
-					<NavLink
-						exact
-						to='/'
-						className={styles.nav_link}
-						activeClassName={styles.nav_link_active}
-					>
-						About Me
-					</NavLink>
-				</li>
-				<li className={styles.nav_item}>
-					<NavLink
-						exact
-						to='/projects'
-						className={styles.nav_link}
-						activeClassName={styles.nav_link_active}
-					>
-						Projects
-					</NavLink>
-				</li>
-			</ul>
+			<NavLink
+				exact
+				to='/'
+				className={styles.nav_link}
+				activeClassName={styles.nav_link_active}
+			>
+				Bio
+			</NavLink>
+			<NavLink
+				exact
+				to='/projects'
+				className={styles.nav_link}
+				activeClassName={styles.nav_link_active}
+			>
+				Projects
+			</NavLink>
 		</nav>
 	)
 }
